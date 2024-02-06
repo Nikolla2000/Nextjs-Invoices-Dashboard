94e006f4ec938e2384e822fddbce06998a85bcc2
Nikola Uzunov <nikollla2000@abv.bg>
2024-02-06 16:44:13 +0200

    Added missing key prop to a map element

--- app/testboard/anothernav.tsx
+++ app/testboard/anothernav.tsx
@@ -4,8 +4,8 @@ export default function Anothernav() {
     return (
         <nav className="bg-blue-500 flex justify-end font-bold">
             <ul className="flex flex-row gap-2 mr-10">
-                {navLinks.map((link) => {
-                    return <li className="text-white p-5 cursor-pointer">{link}</li>
+                {navLinks.map((link, i) => {
+                    return <li className="text-white p-5 cursor-pointer" key={i + 1}>{link}</li>
                 })}
             </ul>
         </nav>
