534397bc2b2cd1782bcc0a5ede10ec7c2720c225
Nikola Uzunov <nikollla2000@abv.bg>
2024-02-21 10:15:02 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,11 @@ let isTurned: boolean = true;
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape}/>
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme.</strong> This is my project for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , deployed by Vercel.
           </p>
           <Link
             href="/login"
